94be80840467e018f5fa6ef09cdbcbc4d098970d
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-09-17 18:19:14 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -102,7 +102,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://muti-agent-ai-research-system.vercel.app",
+        
     ],
     allow_credentials=True,
     allow_methods=[
